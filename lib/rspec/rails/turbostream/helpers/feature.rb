5e4f31a92f892f55772ca2511e146c0a80d62200
Scott Davis <me@sdavis.info>
2023-11-25 23:42:06 -0500

    Credit

--- lib/rspec/rails/turbostream/helpers/feature.rb
+++ lib/rspec/rails/turbostream/helpers/feature.rb
@@ -3,6 +3,7 @@ module RSpec
       module Turbostream
         module Helpers
           module Feature
+            # From: https://discuss.hotwired.dev/t/system-tests-for-turbostreamd-app-without-sleep-1/2956/4
             def wait_for_turbo(timeout = nil)
                 if has_css?('.turbo-progress-bar', visible: true, wait: (0.25).seconds)
                   has_no_css?('.turbo-progress-bar', wait: timeout.presence || 5.seconds)
